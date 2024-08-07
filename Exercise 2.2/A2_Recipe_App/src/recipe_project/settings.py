5167ff04420e9d28b968bc70089abd2f76e6a730
ffferchavez <ffferchavez@gmail.com>
2024-08-07 12:46:08 +0200

    Completed Task:2.3 Django Models

--- Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #RecipesApp-related apps
+    'recipes',
+    'ingredients',
 ]
 
 MIDDLEWARE = [
